f711a4d05268a1f982fedbeae6220c849f3ab035
devcloudarmel <devcloudarmel@gmail.com>
2024-11-04 16:40:12 +0300

    This is it

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,7 +4,7 @@ import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 
-// export const revalidate = 0;
+export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
